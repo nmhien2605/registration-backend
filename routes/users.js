f7e1e0f1d3857e6568a01aac1aebf8e3a614650d
NMHien <nmhien2605@gmail.com>
2022-10-26 21:50:59 +0700

    update api

--- routes/users.js
+++ routes/users.js
@@ -8,31 +8,31 @@ router.get('/', function(req, res, next) {
 
 router.post('/register', function(req, res, next) {
   if (!req.body.username) {
-    return res.status(200).json({
+    return res.status(401).json({
       success: false,
       message: "Invalid. Username is required!!!"
     });
   }
   if (!req.body.email) {
-    return res.status(200).json({
+    return res.status(401).json({
       success: false,
       message: "Invalid. Email is required!!!"
     });
   }
   if (!req.body.password) {
-    return res.status(200).json({
+    return res.status(401).json({
       success: false,
       message: "Invalid. Password is required!!!"
     });
   }
   if (!req.body.confirmPassword) {
-    return res.status(200).json({
+    return res.status(401).json({
       success: false,
       message: "Invalid. Confirm password is required!!!"
     });
   }
   if (req.body.password !== req.body.confirmPassword) {
-    return res.status(200).json({
+    return res.status(401).json({
       success: false,
       message: "Invalid. Password and confirm password must be same!!!"
     });
